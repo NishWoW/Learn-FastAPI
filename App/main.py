2a79ff4c9d186387ee206bd0e7bc87907931432b
NishWoW <nishwayofwill@gmail.com>
2022-06-29 18:53:27 -0400

    changed the message

--- App/main.py
+++ App/main.py
@@ -28,7 +28,7 @@ app.include_router(vote.router)
 
 @app.get("/")
 def root():
-    return {"message": "Welcome to My API!!!"}
+    return {"message": "Welcome to My API. This is exciting!!!"}
 
 
 
